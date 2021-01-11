d16288c2ac963dd4c27b9c9e1ed817a1e6958cc1
duhem-s <duhem-s@mgdis.fr>
2021-01-11 09:00:00 +0100

    deploy

---
+++ precache-manifest.31a519c343c9869509848999088c55c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81df55ec5ccaffcb270c67108a8a86dc",
+    "url": "/dzr-album-releases/channel.html"
+  },
+  {
+    "revision": "c3722c7a594551dfb277",
+    "url": "/dzr-album-releases/css/app.a3de3dbb.css"
+  },
+  {
+    "revision": "31042150a38e6e9a62b6",
+    "url": "/dzr-album-releases/css/chunk-vendors.81e18a23.css"
+  },
+  {
+    "revision": "ebfa1c57983d2ea24256908d93ec2558",
+    "url": "/dzr-album-releases/index.html"
+  },
+  {
+    "revision": "c3722c7a594551dfb277",
+    "url": "/dzr-album-releases/js/app.41cc040b.js"
+  },
+  {
+    "revision": "31042150a38e6e9a62b6",
+    "url": "/dzr-album-releases/js/chunk-vendors.524e3831.js"
+  },
+  {
+    "revision": "ebe61324a2d82ca2358fd6cc077357cb",
+    "url": "/dzr-album-releases/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dzr-album-releases/robots.txt"
+  }
+]);
